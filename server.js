23d6b961284a77416bb5ca862b2b4112679e375c
b1910319 <ldtrinh2001@gmail.com>
2022-09-09 23:08:16 +0700

    Cai dat express chay thong bao xin chao

---
+++ server.js
@@ -0,0 +1,15 @@
+const app = require("./app");
+const config = require("./app/config");
+const PORT = config.app.port;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+}
+);
+
+
+
+
+
+
+
+
